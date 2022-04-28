fd8ae30d0bed2601ce52f469385e7584ea2624e9
Atul Anand <atulanand206@gmail.com>
2022-04-28 11:28:22 +0530

    dp: max profit with buy and sell

---
+++ Java/algorithms/src/test/java/com/creations/algorithms/dp/besttimetobuyandsell/SolutionTest.java
@@ -0,0 +1,27 @@
+package com.creations.algorithms.dp.besttimetobuyandsell;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SolutionTest {
+
+  private static Stream<Arguments> arguments() {
+    return Stream.of(
+        Arguments.of(new int[] {7, 1, 5, 3, 6, 4}, 5),
+        Arguments.of(new int[] {7, 6, 4, 3, 1}, 0),
+        Arguments.of(new int[] {1, 2}, 1),
+        Arguments.of(new int[] {1, 2, 3}, 2),
+        Arguments.of(new int[] {1, 2, 4, 6}, 5));
+  }
+
+  @ParameterizedTest
+  @MethodSource("arguments")
+  public void testKadanes(int[] input, int expected) {
+    int actual = new Solution().maxProfit(input);
+    assertEquals(expected, actual);
+  }
+}
