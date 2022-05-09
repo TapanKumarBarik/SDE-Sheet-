822e5070d82c80624c6377077c6875a8a10eb019
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-05-09 20:10:22 +0530

    Day 1 all Problems

--- A/JavaSolutions/Tapan/Day1/NextPermutation.java
+++ A/JavaSolutions/Tapan/Day1/NextPermutation.java
@@ -4,5 +4,88 @@ public class NextPermutation {
     }
     // https://leetcode.com/problems/next-permutation/
 
+    public void nextPermutation(int[] nums) {
+
+        // find the next decreasing subsequence
+
+        int n = nums.length;
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+
+            // find the number just greater than nums[i]
+            int j = n - 1;
+
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+
+        }
+
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+
+        while (i <= j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     // www.codingninjas.com/codestudio/problems/893046?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+
+    public static ArrayList<Integer> nextPermutation(ArrayList<Integer> p) {
+        // Write your code here.
+
+        int n = p.size();
+
+        int i = n - 2;
+
+        while (i >= 0 && p.get(i) >= p.get(i + 1)) {
+            i--;
+        }
+
+        if (i >= 0) {
+
+            int j = n - 1;
+
+            while (j >= 0 && p.get(i) >= p.get(j)) {
+                j--;
+            }
+            swap(p, i, j);
+        }
+        reverse(p, i + 1, n - 1);
+
+        return p;
+    }
+
+    private static void swap(ArrayList<Integer> p, int i, int j) {
+        int temp = p.get(i);
+        p.set(i, p.get(j));
+        p.set(j, temp);
+    }
+
+    private static void reverse(ArrayList<Integer> p, int i, int j) {
+
+        while (i < j) {
+            swap(p, i, j);
+            i++;
+            j--;
+        }
+    }
+
 }
--- A/JavaSolutions/Tapan/Day1/Readme.md
+++ A/JavaSolutions/Tapan/Day1/Readme.md
@@ -12,11 +12,15 @@
 
 #### https://leetcode.com/problems/pascals-triangle/
 
+#### https://www.codingninjas.com/codestudio/problems/1089580?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
 ## Problem 3
 
 ### Maximum SubArray
 
 #### https://leetcode.com/problems/maximum-subarray/
+
+#### https://www.codingninjas.com/codestudio/problems/630526?leftPanelTab=0
 
 ## Problem 4
 
@@ -24,14 +28,20 @@
 
 #### https://leetcode.com/problems/sort-colors/
 
+#### https://www.codingninjas.com/codestudio/problems/631055?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
 ## Problem 5
 
 ### Best time to buy and sell stock 1
 
 #### https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+#### https://www.codingninjas.com/codestudio/problems/893405?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
 ## Problem 6
 
 ### Next Permutation
 
 #### https://leetcode.com/problems/next-permutation/
+
+#### www.codingninjas.com/codestudio/problems/893046?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
